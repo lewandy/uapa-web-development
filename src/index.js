0d9f8bb705e475d5534047e2997481e7f1764691
Lewandy Dilone <lewandydilone1@live.com>
2021-05-10 22:30:14 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+const calculateButton = document.getElementById("calculate-button");
+
+calculateButton.onclick = () => {
+  const OPERATION = document.querySelector("input[name = operation]:checked")
+    .value;
+};
